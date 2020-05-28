0102f6a04def24b19efc353a2abf4b95971c4811
Roman <roman.tremmel@gmail.com>
2020-05-28 11:09:50 +0200

    Add files via upload

---
+++ global.R
@@ -0,0 +1,22 @@
+lapply(c("shiny", "tidyverse", "DT"), function(x) if(!require(x,character.only = TRUE)) install.packages(x))
+
+# all_data <- list.files(".", recursive = T, full.names = T) %>%
+#   grep("csv/", ., value = T) %>%
+#   grep("txt", ., value = T, invert = T) %>%
+#   set_names(str_split(., pattern = "/") %>% map_chr(last)) %>%
+#   map(read.csv, comment.char = "#", stringsAsFactors =F) %>%
+#   map(as_tibble)
+# all_data <- all_data[order(names(all_data))][c(2,1,3:6)]
+# saveRDS(all_data, "HTA2.0Transcript_Cluster_Annotations.rds")
+
+all_data <- readRDS("HTA2.0Transcript_Cluster_Annotations.rds")
+selection <- all_data$`HTA-2_0.na36.hg19.transcript.csv` %>% 
+  split(.$seqname) %>% 
+  map(pull, transcript_cluster_id)
+
+
+
+
+
+
+
---
+++ server.R
@@ -0,0 +1,56 @@
+
+server <- function(input, output, session) {
+
+updateSelectizeInput(session = session, inputId = 'id', choices = selection, server = TRUE)
+
+
+data <- reactive({
+    req(input$id)
+    all_data %>%
+        map(~filter(., transcript_cluster_id %in% input$id))})
+
+output$DTS_probe_gene <- renderDataTable({
+    req(input$id)
+    data() %>% 
+        map(~select(.,1, gene_assignment)) %>% 
+        bind_rows(.id = "version") %>% 
+        pivot_wider(names_from = version, values_from = gene_assignment) %>% 
+        datatable(extensions = 'Buttons', 
+                  options = list(autoWidth = TRUE,
+                                 dom = 'Bfrtip'))
+     })
+
+output$DTS_probe_mRNA <- renderDataTable({
+    req(input$id)
+    data() %>% 
+        map(~select(.,1, mrna_assignment)) %>% 
+        bind_rows(.id = "version") %>% 
+        pivot_wider(names_from = version, values_from = mrna_assignment) %>% 
+        datatable(extensions = 'Buttons', 
+                  options = list(autoWidth = TRUE,
+                                 dom = 'Bfrtip'))
+     })
+
+
+
+output$DTS_probe_across_releases <- renderDataTable({
+  
+  data() %>% 
+   map(~mutate_at(., vars(start, stop), as.numeric)) %>% 
+    bind_rows(.id = "Release") %>% 
+    datatable(extensions = 'Buttons', 
+              filter =  list(position = 'top', clear = TRUE),
+              options = list(autoWidth = TRUE,
+                             dom = 'Bfrtip'))
+})
+}
+
+
+
+
+
+    
+
+
+
+
