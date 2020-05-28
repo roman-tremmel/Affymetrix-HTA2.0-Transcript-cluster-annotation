ada00b8b42fb8881e9171186e1072a6f463afcce
Roman <roman.tremmel@gmail.com>
2020-05-28 13:13:19 +0200

    Add files via upload

--- global.R
+++ global.R
@@ -9,14 +9,8 @@ lapply(c("shiny", "tidyverse", "DT"), function(x) if(!require(x,character.only =
 # all_data <- all_data[order(names(all_data))][c(2,1,3:6)]
 # saveRDS(all_data, "HTA2.0Transcript_Cluster_Annotations.rds")
 
-all_data <- readRDS("HTA2.0Transcript_Cluster_Annotations.rds")
+all_data <- readRDS("HTA2.0Transcript_Cluster_Annotations_snippet.rds")
 selection <- all_data$`HTA-2_0.na36.hg19.transcript.csv` %>% 
   split(.$seqname) %>% 
   map(pull, transcript_cluster_id)
-
-
-
-
-
-
 
